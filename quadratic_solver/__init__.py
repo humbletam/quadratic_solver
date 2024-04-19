06b9fdccf1190b768c328bbf7feaf7877d4819ec
Rustam Zaloldinov <ruzalo@ttu.ee>
2024-04-19 19:12:36 +0300

    first commit

---
+++ quadratic_solver/__init__.py
@@ -0,0 +1,8 @@
+import cmath
+
+
+def solve_quadratic(a, b, c):
+    discriminant = cmath.sqrt(b**2 - 4*a*c)
+    x1 = (-b + discriminant) / (2 * a)
+    x2 = (-b - discriminant) / (2 * a)
+    return (x1, x2)
